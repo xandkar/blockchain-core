13ce59fdd9e4e869898c5df7154efb219fec6e46
Louis Thiery <thiery.louis@gmail.com>
2020-08-26 13:26:51 -0700

    yes it is causally correct

--- src/state_channel/blockchain_state_channels_client.erl
+++ src/state_channel/blockchain_state_channels_client.erl
@@ -732,28 +732,28 @@ is_active_sc(SC, #state{chain=Chain}) ->
 
 -spec is_causally_correct_sc(SC :: blockchain_state_channel_v1:state_channel(),
                              State :: state()) -> boolean().
-is_causally_correct_sc(SC, #state{pubkey_bin=PubkeyBin}=State) ->
-    SCID = blockchain_state_channel_v1:id(SC),
 
-    case get_state_channels(SCID, State) of
-        {error, not_found} ->
-            true;
-        {error, _} ->
-            lager:error("rocks blew up"),
-            %% rocks blew up
-            false;
-        {ok, [KnownSC]} ->
-            %% Check if SC is causally correct
-            Check = (conflict /= blockchain_state_channel_v1:quick_compare_causality(KnownSC, SC, PubkeyBin)),
-            lager:info("causality check: ~p, this sc_id: ~p, known_sc_id: ~p",
-                       [Check, SCID, blockchain_state_channel_v1:id(KnownSC)]),
-            Check;
-        {ok, KnownSCs} ->
-            lager:error("multiple copies of state channels for id: ~p, found: ~p", [SCID, KnownSCs]),
-            %% We have a conflict among incoming state channels
-            ok = debug_multiple_scs(SC, KnownSCs),
-            false
-    end.
+is_causally_correct_sc(_SC, _State) ->
+    true.
+%%    case get_state_channels(SCID, State) of
+%%        {error, not_found} ->
+%%            true;
+%%        {error, _} ->
+%%            lager:error("rocks blew up"),
+%%            %% rocks blew up
+%%            false;
+%%        {ok, [KnownSC]} ->
+%%            %% Check if SC is causally correct
+%%            Check = (conflict /= blockchain_state_channel_v1:quick_compare_causality(KnownSC, SC, PubkeyBin)),
+%%            lager:info("causality check: ~p, this sc_id: ~p, known_sc_id: ~p",
+%%                       [Check, SCID, blockchain_state_channel_v1:id(KnownSC)]),
+%%            Check;
+%%        {ok, KnownSCs} ->
+%%            lager:error("multiple copies of state channels for id: ~p, found: ~p", [SCID, KnownSCs]),
+%%            %% We have a conflict among incoming state channels
+%%            ok = debug_multiple_scs(SC, KnownSCs),
+%%            false
+%%    end.
 
 is_overspent_sc(SC, State=#state{chain=Chain}) ->
     SCID = blockchain_state_channel_v1:id(SC),
