accbad999a13550506b90ea8e4af28420052fa92
Siraaj Khandkar <siraaj@khandkar.net>
2022-11-29 21:33:38 -0500

    Terribly export accounts to CSV

--- src/ledger/v1/blockchain_ledger_exporter_v1.erl
+++ src/ledger/v1/blockchain_ledger_exporter_v1.erl
@@ -6,7 +6,8 @@
 -module(blockchain_ledger_exporter_v1).
 
 -export([
-    export/1
+    export/1,
+    export_accounts_csv/2
 ]).
 
 -spec export(blockchain_ledger_v1:ledger()) -> any().
@@ -29,6 +30,108 @@ export_accounts(Ledger) ->
         [],
         maps:to_list(blockchain_ledger_v1:entries(Ledger))
     ).
+
+-spec export_accounts_csv(
+    blockchain_ledger_v1:ledger(),
+    file:name_all()
+) -> ok.
+export_accounts_csv(Ledger, OutFilePatch) ->
+    {ok, File} = file:open(OutFilePatch, [write]),
+    HeadRow =
+        [
+            "status",
+            "net-type",
+            "key-type",
+            "key-size",
+            "pubkey-helium-b58",
+            "pubkey-solana-b58",
+            "bones",
+            "hnt"
+        ],
+    ok = file:write(File, [lists:join(",", HeadRow), "\n"]),
+    lists:foldl(
+        fun({<<PubKeyHelium/binary>>, Entry}, ok) ->
+            <<NetTypeInt:4, KeyTypeInt:4, PubKey/binary>> = PubKeyHelium,
+            Net =
+                case NetTypeInt of
+                    0 -> mainnet;
+                    1 -> testnet;
+                    _ -> invalid
+                end,
+            PubKeyHeliumB58 = libp2p_crypto:bin_to_b58(PubKeyHelium),
+            PubKeySolanaB58 = bs58(PubKey),
+            BalanceBones = blockchain_ledger_entry_v1:balance(Entry),
+            BalanceHNT = BalanceBones / 100_000_000,
+            KeySize = integer_to_list(byte_size(PubKey)),
+            NetTypeIntStr = integer_to_list(NetTypeInt),
+            KeyTypeIntStr = integer_to_list(KeyTypeInt),
+            MakeRow =
+                fun (Status, KeyTypeStr) ->
+                        [
+                            Status,
+                            NetTypeIntStr,
+                            KeyTypeStr,
+                            KeySize,
+                            PubKeyHeliumB58,
+                            PubKeySolanaB58,
+                            integer_to_list(BalanceBones),
+                            io_lib:format("~.10f", [BalanceHNT])
+                        ]
+                end,
+            Row =
+                case lists:member(Net, [mainnet, testnet]) of
+                    false ->
+                        MakeRow("err-net", KeyTypeIntStr);
+                    true ->
+                        case libp2p_crypto:bin_to_pubkey_2(Net, PubKeyHelium) of
+                            {ok, {KeyTypeAtom, _}} ->
+                                MakeRow("ok", atom_to_list(KeyTypeAtom));
+                            {error, {bad_key, _}} ->
+                                MakeRow("err-key", KeyTypeIntStr)
+                        end
+                end,
+            ok = file:write(File, [lists:join(",", Row), "\n"])
+        end,
+        ok,
+        maps:to_list(blockchain_ledger_v1:entries(Ledger))
+    ),
+    ok = file:close(File).
+
+bs58(Data0) ->
+    ScratchFilePath = scratch_file_path(),
+    ok = file:write_file(ScratchFilePath, Data0),
+    ExePath = "/home/xand/.cargo/bin/bs58",
+    % XXX bs58 only accepts stdin, but sending it via port didn't work.
+    {0, Data1} = os_cmd(ExePath ++ " < " ++ ScratchFilePath),
+    Data1.
+
+scratch_file_path() ->
+    Key = {?MODULE, scratch_file_path},
+    case get(Key) of
+        undefined ->
+            {0, Path0} = os_cmd("mktemp"),
+            Path = Path0 -- "\n",
+            put(Key, Path),
+            Path;
+        Path ->
+            Path
+    end.
+
+os_cmd(Cmd) ->
+    PortId = open_port({spawn, Cmd}, [stream, exit_status, use_stdio, in, eof]),
+    os_cmd_collect(PortId, []).
+
+os_cmd_collect(PortId, Data) ->
+    receive
+        {PortId, {data, Datum}} ->
+            os_cmd_collect(PortId, [Datum | Data]);
+        {PortId, eof} ->
+            port_close(PortId),
+            receive
+                {PortId, {exit_status, ExitStatusCode}} ->
+                    {ExitStatusCode, lists:flatten(lists:reverse(Data))}
+            end
+    end.
 
 -spec export_gateways(blockchain_ledger_v1:ledger()) -> list().
 export_gateways(Ledger) ->
